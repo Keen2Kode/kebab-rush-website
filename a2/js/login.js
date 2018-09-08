6bb9fac217daef696f305a70524fdf0db1c97172
s3717497 <41359510+s3717497@users.noreply.github.com>
2018-09-08 18:38:48 +1000

    New code practices and emailCheck() changes
    place event listeners and place code in HTML footer.
    Email pattern updated to be more strict.
    changed the alert() method in formValidate()

--- a2/js/login.js
+++ a2/js/login.js
@@ -1,3 +1,7 @@
+// ensure that this code is placed in the footer
+// add event listeners in assignment 3
+
+
 function getId(id) {
     return document.getElementById(id);
 }  
@@ -6,16 +10,19 @@ function formValidate(){
   	var errors= 0;
   	if (emailCheck()    == false)	errors++;
     if (passwordCheck() == false)	errors++;
-    if (errors>0){
-        alert("We've found " + errors + " errors with your login information");
-    	return false;
+    
+    switch (errors) {
+        case 1 : alert("We've found " + errors + " error with your login information");     return false;
+        case 2 : alert("We've found " + errors + " errors with your login information");    return false;
+        default: return true;
     }
-    return true;
+    
 }
   
 
 
 // common check method
+// displays errorMsg to user
 function check(id,errorMsg) {
     getId(id).placeholder = errorMsg;
     
@@ -39,10 +46,12 @@ function passwordCheck() {
     return check('password',errorMsg);
 }
 
+// returns true if the errorMsg is empty
+// this means there is no error
 function emailCheck() {
     var errorMsg = "";
     var email = getId('email').value;
-    var pattern = /^[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-.]+$/;
+    var pattern = /[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-.]+\.((com)|(edu)|(org)|(net)|(au))/;
 
     // input does not match pattern
     if (!pattern.test(email))    errorMsg = "Wrong email format";
