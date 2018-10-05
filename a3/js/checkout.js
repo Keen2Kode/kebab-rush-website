7207c4fb7b90efbf616a7345bcfe40e29eaeb516
s3717497 <41359510+s3717497@users.noreply.github.com>
2018-10-05 17:48:37 +1000

    Update checkout.js

--- a3/js/checkout.js
+++ a3/js/checkout.js
@@ -3,14 +3,18 @@
 // http://jsfiddle.net/AbSh2/590/
 
 
-function getId(id) {
-    return document.getElementById(id);
+function getName(name) {
+    return document.getElementByName(name);
 }  
   
 function formValidate(){
     var errors= 0;
+    if (nameCheck()     == false)   errors++;
    	if (emailCheck()    == false)	errors++;
-    if (passwordCheck() == false)	errors++;
+    if (addressCheck()  == false)   errors++;
+    if (phoneCheck()    == false)   errors++;
+    if (cardCheck()     == false)   errors++;
+    if (expiryCheck()   == false)   errors++;
      
     switch (errors) {
         case 1 : alert("We've found " + errors + " error with your login information");     return false;
@@ -22,38 +26,87 @@ function formValidate(){
 
 // common check method
 // displays errorMsg to user
-function check(id,errorMsg) {
-    getId(id).placeholder = errorMsg;
+function check(name,errorMsg) {
+    var errorName = name + "Error";
+    getName(errorName).innerHTML = errorMsg;
     
-    //var errorId = id + "Error"; assignment 3
+    
     if (errorMsg == "")	
     	return true;
     else
-        getId(id).value = "";
+        getName(name).value = "";
         return false;
 }
     
-// returns true if the errorMsg is empty
-// this means there is no error
-function passwordCheck() {
-    var errorMsg = "";
-    var password = getId("password").value;
-    
-    if (password.length < 3)	errorMsg = "Min. 3 characters allowed";
-    if (password.includes(" "))	errorMsg = "Spaces not allowed";
-    
-    return check('password',errorMsg);
-}
+
+
+
+
+
+
+
+
+
+
 
 // returns true if the errorMsg is empty
 // this means there is no error
+function nameCheck() {
+    var name = getName('name').value;
+    var pattern = /[a-zA-Z_\-]+/;
+    
+    // input does not match pattern
+    var errorMsg = (pattern.test(name)) ? "" : "Please enter a valid name";
+    return check('name',errorMsg);
+}
+
+
 function emailCheck() {
-    var errorMsg = "";
-    var email = getId('email').value;
+    var email = getName('email').value;
     var pattern = /[a-zA-Z0-9_\-.]+@[a-zA-Z0-9\-.]+\.((com)|(edu)|(org)|(net)|(au))/;
 
     // input does not match pattern
-    if (!pattern.test(email))    errorMsg = "Wrong email format";
+    var errorMsg = "";
+    if (!pattern.test(email))    errorMsg = "Wrong email format (should be in the format abcedg@email.com)";
     
     return check('email',errorMsg);
 }
+
+function addressCheck() {
+    var address = getName('address').value;
+    var pattern = /customAddressRegex/; 
+    
+    // input does not match pattern
+    var errorMsg = (pattern.test(address)) ? "" : "Custom address error";
+    return check('address',errorMsg);
+}
+
+function phoneCheck() {
+    var phone = getName('phone').value;
+    var pattern = /customPhoneRegex/;
+    
+    // input does not match pattern
+    var errorMsg = (pattern.test(phone)) ? "" : "Custom phone error";
+    return check('phone',errorMsg);
+}
+
+function cardCheck() {
+    var card = getName('card').value;
+    var pattern = /customCardRegex/;
+    
+    // input does not match pattern
+    var errorMsg = (pattern.test(card)) ? "" : "Custom card error";
+    return check('card',errorMsg);
+}
+
+function expiryCheck() {
+    var expiry = getName('expiry').value;
+    var pattern = /customExpiryRegex/;
+    
+    // input does not match pattern
+    var errorMsg = (pattern.test(expiry)) ? "" : "Custom expiry error";
+    return check('expiry',errorMsg);
+}
+
+
+
